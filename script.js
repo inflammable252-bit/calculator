b3b0fdffb05dc7241298d9f808c47bdc504b6f43
MT <221427704+inflammable252-bit@users.noreply.github>
2025-12-05 04:59:08 -0800

    Add DOM selectors and functions to update screen Numbers and operators can be input and displayed on the screen. DEL and C will delete and clear input. Issues: repeat decimals and operators need to be prevented, operate() needs to be triggered on = and consecutive operator entry

--- script.js
+++ script.js
@@ -18,10 +18,13 @@ const power = function (a, b) {
     return a ** b
 }
 
-function operate(a, operator, b) {
+function operate(string, operator) {
+    let operatorIndex = string.indexOf(operator)
+    a = string.slice(0, operatorIndex)
+    b = string.slice(operatorIndex)
     switch (operator) {
         case "+":
-            return add(a,b)
+            screenText = add(a,b)
         case "-":
             return subtract(a,b)
         case "x":
@@ -32,4 +35,48 @@ function operate(a, operator, b) {
            return power(a,b)
     }
 }
-console.log(operate(5,"+",1))
+
+let screen = document.querySelector(".screen")
+let screenText = ""
+let keys = document.querySelectorAll(".numkey")
+let float = document.querySelector("#float")
+
+let operators = document.querySelectorAll("#op")
+let util = document.querySelectorAll("#util")
+
+function updateScreen(key) {
+    screenText += key.textContent
+    screen.textContent = screenText
+}
+
+function clearScreen() {
+    screenText = ""
+    screen.textContent = ""
+}
+
+keys.forEach((key) => {
+    key.addEventListener("click", () => {
+        updateScreen(key)
+
+    })
+})
+// if ((screenText.charAt(screenText.length - 1 == ".") && (key == float))) {return}
+
+operators.forEach((key) => {
+    key.addEventListener("click", () => {
+        updateScreen(key)
+
+    })
+})
+
+util.forEach((key) => {
+    key.addEventListener("click", () => {
+        switch (key.textContent) {
+            case "C":
+                clearScreen()
+            case "DEL":
+                screenText = screenText.slice(0, (screenText.length - 1))
+                screen.textContent = screenText
+        }
+    })
+})
